6f592105f4231e021bfdb2f0a89ad73b8c0f402f
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2022-11-01 13:28:25 -0400

    recurssion

--- RecursionAndFibonacci.js
+++ RecursionAndFibonacci.js
@@ -10,3 +10,65 @@ A simple base case (or cases) - which is a terminating scenario that does not us
 A set of rules that reduce all other cases towards the base case
 Recursion is not yet in our coding tool kit. Solving things recursively takes practice and may feel quite odd at first.
  */
+
+// CLASS
+const numsToSum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sumNumbers = (numsArr, sum = 0) => {
+  // base case - until the condition is met and no numbers in the array to add
+  if (numsArr.length === 0) {
+    return sum;
+  }
+  // after a number is added its then removed from the array using the shift method
+  sum += numsArr.shift();
+  // recursively calling the function again and again till our condition is met
+  console.log(`${sum}`); // to see sum in the process
+  return sumNumbers(numsArr, sum);
+};
+
+console.log(sumNumbers(numsToSum));
+
+// NOTES
+const happyNewYear = (num) => {
+  // base case - where we end the funciton
+  if (num === 0) {
+    console.log("HAPPY NEW YEAR");
+  } else {
+    // move the funciton towards the base case
+    console.log(`${num}.....`);
+    happyNewYear(num - 1);
+  }
+};
+console.log(happyNewYear(5));
+
+// CLASS
+const testCase1 = 10;
+const testCase2 = 9;
+
+const isEven = (num) => {
+  // base case
+  if (num === 0) {
+    return "the number is even";
+  }
+  // base case
+  if (num === 1) {
+    return "the number is odd";
+  }
+  // recursively calling the function
+  return isEven(num - 2);
+};
+
+console.log(isEven(testCase1));
+console.log(isEven(testCase2));
+
+// Lets try factorial;
+
+const factorial = (n) => {
+  // base case
+  if (n == 1 || n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+};
+console.log(factorial(4));
